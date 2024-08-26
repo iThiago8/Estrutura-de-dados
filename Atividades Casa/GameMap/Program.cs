22be2b33d951446a4cac271b75af302a71e80723
iThiago8 <thiago.pedro.padilha899@gmail.com>
2024-08-26 17:14:40 -0300

    GameMap - Multidimensional arrays

---
+++ Atividades Casa/GameMap/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+Console.OutputEncoding = UTF8Encoding.UTF8;
+
+TerrainEnum[,] map =
+{
+    {
+        TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND,
+    },
+    {
+        TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER,
+    },    
+    {
+        TerrainEnum.WALL, TerrainEnum.WALL, TerrainEnum.WALL,
+    },    
+    {
+        TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS,
+    }
+}; 
+
+for (int row = 0; row < map.GetLength(0); row++)
+{
+    for (int column = 0; column < map.GetLength(1); column++)
+    {
+        Console.ForegroundColor = map[row, column].GetColor();
+        Console.Write(map[row, column].GetChar() + " ");
+    }
+    Console.WriteLine();
+}
+Console.ForegroundColor = ConsoleColor.Gray;
