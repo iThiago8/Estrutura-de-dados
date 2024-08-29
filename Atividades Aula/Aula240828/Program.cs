fad04791c4828afc288c9e1e2e249d2a629b4fd6
iThiago8 <thiago.pedro.padilha899@gmail.com>
2024-08-28 22:00:51 -0300

    Aula240828

---
+++ Atividades Aula/Aula240828/Program.cs
@@ -0,0 +1,25 @@
+﻿List<int> numerosPares = [];
+List<int> numerosImpares = [];
+
+for (int i = 0; numerosPares.Count <= 100 && numerosImpares.Count <= 100;  i++)
+{
+    if (i % 2 == 0)
+    {
+        numerosPares.Add(i);
+    }
+    else
+    {
+        numerosImpares.Add(i);
+    }
+}
+
+Console.Write("Números pares: ");
+foreach (var i in  numerosPares)
+    Console.Write($"{i},");
+
+Console.WriteLine();
+Console.WriteLine();
+Console.Write("Números ímpares: ");
+foreach (var i in numerosImpares)
+    Console.Write($"{i},");
+
