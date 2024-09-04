a023936aa1085cd356ca2215ea0f4a74cc88f315
iThiago8 <thiago.pedro.padilha899@gmail.com>
2024-09-04 20:49:27 -0300

    Pré intervalo

---
+++ Atividades Aula/JaggedArray/Program.cs
@@ -0,0 +1,10 @@
+﻿//um jaggedArray é também conhecido como vetor de vetores
+
+//[][] Significa que é um vetor de vetores e neste caso [4][] quer dizer que é um um vetor de 4 posições com arrays indefinidos
+int[][] numbers = new int[4][];
+
+numbers[0] = new int[] { 1, 4, 3 };
+numbers[1] = new int[] { 2, 8 };
+numbers[2] = new int[] { 1, 4, 3, 10 };
+numbers[3] = new int[] { 0 };
+numbers[4] = null!;
