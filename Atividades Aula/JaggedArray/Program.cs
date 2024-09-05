53b3aee097ee1cafad6ab17a922eeb5413460725
iThiago8 <thiago.pedro.padilha899@gmail.com>
2024-09-04 21:22:25 -0300

    Jagged array

--- Atividades Aula/JaggedArray/Program.cs
+++ Atividades Aula/JaggedArray/Program.cs
@@ -8,3 +8,11 @@ numbers[1] = new int[] { 2, 8 };
 numbers[2] = new int[] { 1, 4, 3, 10 };
 numbers[3] = new int[] { 0 };
 numbers[4] = null!;
+
+//Para obter valor de jagged array
+int valor = numbers[0][2];
+Console.WriteLine(valor);
+
+//Atribuir valor em um jagged array
+numbers[1][2] = 35;
+Console.WriteLine(numbers[1][2]);
