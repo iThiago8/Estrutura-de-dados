969bce06f3d938c9f654dffbba0c68f17ab81bd7
iThiago8 <thiago.pedro.padilha899@gmail.com>
2024-09-04 22:02:59 -0300

    Final aula

---
+++ Atividades Aula/ListaSimples/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+//Utilizando a lista simples
+ArrayList arrList = [];
+arrList.Add(5);
+arrList.Add("Thiago");
+arrList.AddRange(new int[] { 1, 2, 3 });
+//Método Add() coloca no final da lista
+
+arrList.Insert(1, 15);
+//Método Insert() coloca na posição especificada e empurra os que estão à direita 
+
+arrList[1] = 4;
+//Via indexador, o valor antigo é substituído
+
+//Lendo valores da lista
+object primeiro = arrList[0]!;
+int quarto = (int)arrList[3]!;
+
+// percorrendo a lista com foreach
+foreach (var obj in arrList)
+{
+    Console.WriteLine(obj);
+}
+
+
