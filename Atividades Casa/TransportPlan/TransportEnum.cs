48d2be73497ede7a1e9d7dd036cd774a4ae968e9
Thiago Pedro Padilha <thiago.pedro.padilha899@gmail.com>
2024-09-10 21:28:44 -0300

    Iniciado atv de transporte

---
+++ Atividades Casa/TransportPlan/TransportEnum.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportPlan
+{
+    public enum TransportEnum
+    {
+        CAR,
+        BUS,
+        SUBWAY,
+        BIKE,
+        WALK
+    }
+
+    public static class TransportEnumExtensions
+    {
+        public static char GetChar(this TransportEnum transport)
+        {
+            switch (transport)
+            {
+                case TransportEnum.CAR: return 'C';
+                case TransportEnum.BUS: return 'U';
+                case TransportEnum.BIKE: return 'B';
+                default: return 'W';
+            }
+        }
+
+        public static ConsoleColor GetColor(this TransportEnum transport)
+        {
+            switch (transport)
+            {
+                case TransportEnum.CAR: return 'C';
+                case TransportEnum.BUS: return 'U';
+                case TransportEnum.BIKE: return 'B';
+                default: return 'W';
+            }
+        }
+    }
+
+}
