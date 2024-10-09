69f419859ec3c9697572a61132c26daf976db462
Thiago Pedro Padilha <thiago.pedro.padilha899@gmail.com>
2024-10-08 22:06:32 -0300

    nada funciona

--- Atividades Casa/HashTable/Program.cs
+++ Atividades Casa/HashTable/Program.cs
@@ -8,24 +8,36 @@ string[] merchandises = { "Banana", "Batata", "Macarrão", "Leite", "Sabonete", 
 Hashtable shelves = [];
 Queue<Product> products = [];
 
-for (int i = 0; i < 99; i++)
+for (int i = 0; i < 27;)
 {
     products.Enqueue(new Product()
     {
         Id = ++i,
         Name = merchandises[random.Next(0, 7)]
     });
-
 }
 
 for (int i = 1; i < 10; i++)
 {
-    shelves.Add($"A{i}", products.Dequeue());
-    shelves.Add($"B{i}", products.Dequeue());
-    shelves.Add($"C{i}", products.Dequeue());
+    shelves.Add(products.Dequeue(), $"A{i}");
+    shelves.Add(products.Dequeue(), $"B{i}");
+    shelves.Add(products.Dequeue(), $"C{i}");
 }
 
-foreach (DictionaryEntry item in shelves)
+Console.WriteLine("Escreva o código do produto");
+bool parsing = false;
+int productId = 0;
+
+while(!parsing)
 {
-    Console.WriteLine($"{item.Key} - {item.Value?.ToString()}");
+    parsing = int.TryParse(Console.ReadLine(), out productId);
+    if (!parsing)
+        Console.WriteLine("Favor inserir um valor válido");
 }
+
+Product keyToFind = new Product() { Id = productId };
+if (shelves[keyToFind] != null)
+    Console.WriteLine($"O produto está na prateleira: {shelves[keyToFind]}");
+else
+    Console.WriteLine($"O produto não foi encontrado");
+
