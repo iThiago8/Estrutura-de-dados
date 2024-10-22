7c87b5ae366cfae136fb6ad3f6bc6ea6b5227129
Thiago Pedro Padilha <411730@unoesc.lan>
2024-10-22 20:59:11 -0300

    Contexto shellsort iniciado

--- Atividades Casa/ShellSort/Program.cs
+++ Atividades Casa/ShellSort/Program.cs
@@ -1,6 +1,15 @@
 ﻿using ShellSort;
 
-int[] arr = { 12, 43, 23, 423, 11, 214 };
+System.Console.WriteLine("MENU");
+System.Console.WriteLine("Digite 1 para digitar novos produtos");
+System.Console.WriteLine("Digite 2 para ordenar os produtos já cadastrados");
+
+
+bool conversion = int.TryParse(Console.ReadLine(), out int option);
+
+switch 
+
+int[] arr = [12, 43, 23, 423, 11, 214];
 
 
 ShellSort.ShellSort.Sort(arr);
